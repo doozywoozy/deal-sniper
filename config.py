e859795765a1b718a833c09cf749e85ce78733e2
doozywoozy <jakob.joneborg@icloud.com>
2025-09-10 22:49:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@ OLLAMA_BASE_URL = "http://localhost:11434"
 AI_MODEL = "mistral:7b"
 
 # Scraping settings
-MAX_PAGES_TO_SCRAPE = 5  # Number of pages to scrape per search
+MAX_PAGES_TO_SCRAPE = 3  # Number of pages to scrape per search
 REQUEST_DELAY = 1  # Delay between requests in seconds
 
 # Profitability thresholds (in SEK)
 PRICE_THRESHOLDS = {
     "rtx_3080": 8000,
-    "stationary_computers": 20000,
+    "stationary_computers": 10000,
     "xeon_workstation": 5000
 }
 
